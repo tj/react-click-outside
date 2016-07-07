c5f0fc36d9371fd86d03a3e66d3caf803b0d848e
Marc Binder <marcandrebinder@gmail.com>
2016-07-07 09:31:08 +0200

    Do not pass 'onClickOutside' to the <div />-wrapper

--- build/index.js
+++ build/index.js
@@ -4,13 +4,19 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _react2 = require('react');
+
+var _react3 = _interopRequireDefault(_react2);
+
+var _reactTransformHmr3 = require('react-transform-hmr');
+
+var _reactTransformHmr4 = _interopRequireDefault(_reactTransformHmr3);
+
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var _react = require('react');
-
-var _react2 = _interopRequireDefault(_react);
+var _class, _temp2;
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -22,7 +28,26 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var ClickOutside = function (_Component) {
+var _components = {
+  ClickOutside: {
+    displayName: 'ClickOutside'
+  }
+};
+
+var _reactTransformHmr2 = (0, _reactTransformHmr4.default)({
+  filename: 'index.js',
+  components: _components,
+  locals: [module],
+  imports: [_react3.default]
+});
+
+function _wrapComponent(id) {
+  return function (Component) {
+    return _reactTransformHmr2(Component, id);
+  };
+}
+
+var ClickOutside = _wrapComponent('ClickOutside')((_temp2 = _class = function (_Component) {
   _inherits(ClickOutside, _Component);
 
   function ClickOutside() {
@@ -49,10 +74,11 @@ var ClickOutside = function (_Component) {
     value: function render() {
       var _props = this.props;
       var children = _props.children;
+      var onClickOutside = _props.onClickOutside;
 
-      var props = _objectWithoutProperties(_props, ['children']);
+      var props = _objectWithoutProperties(_props, ['children', 'onClickOutside']);
 
-      return _react2.default.createElement(
+      return _react3.default.createElement(
         'div',
         _extends({}, props, { ref: 'container' }),
         children
@@ -71,9 +97,8 @@ var ClickOutside = function (_Component) {
   }]);
 
   return ClickOutside;
-}(_react.Component);
+}(_react2.Component), _class.propTypes = {
+  onClickOutside: _react2.PropTypes.func.isRequired
+}, _temp2));
 
-ClickOutside.propTypes = {
-  onClickOutside: _react.PropTypes.func.isRequired
-};
 exports.default = ClickOutside;
