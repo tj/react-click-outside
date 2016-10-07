260772b58c97eb4eec7a2933304360973ba7065a
Jeffrey Horn <jeffreyh@zillowgroup.com>
2016-10-07 15:45:54 -0700

    add shouldClickOutside

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ import React, { Component, PropTypes } from 'react'
 
 export default class ClickOutside extends Component {
   static propTypes = {
-    onClickOutside: PropTypes.func.isRequired
+    onClickOutside: PropTypes.func.isRequired,
+    shouldClickOutside: PropTypes.func
+  };
+
+  static defaultProps = {
+    shouldClickOutside: () => true
   };
 
   render() {
-    const { children, onClickOutside, ...props } = this.props
+    const { children, onClickOutside, shouldClickOutside, ...props } = this.props
     return <div {...props} ref={ref => this.container = ref}>{children}</div>
   }
 
@@ -20,8 +25,8 @@ export default class ClickOutside extends Component {
   }
 
   handle = e => {
-    const { onClickOutside } = this.props
+    const { onClickOutside, shouldClickOutside } = this.props
     const el = this.container
-    if (!el.contains(e.target)) onClickOutside(e)
+    if (!el.contains(e.target) && shouldClickOutside(e)) onClickOutside(e)
   };
 }
