c01d090f7d8e2382f9ead11f7245b7207ceccc81
dudiq <idudiq@gmail.com>
2018-02-16 09:28:07 +0300

    fix scroll prevent for touch devices

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
+const THRESHOLD = 3;
+
+function inThreshold(pos1, pos2) {
+  return (Math.abs(pos1.x - pos2.x) < THRESHOLD) &&
+    (Math.abs(pos1.y - pos2.y) < THRESHOLD);
+}
+
+function setPos(pos, e) {
+  let touches = e.touches ? e.touches[0] : null;
+  if (!touches) {
+    touches = e.changedTouches ? e.changedTouches[0] : null;
+  }
+  pos.x = touches ? touches.pageX : e.clientX;
+  pos.y = touches ? touches.pageY : e.clientY;
+  return pos;
+}
+
+
 export default class ClickOutside extends Component {
   static propTypes = {
     onClickOutside: PropTypes.func.isRequired
@@ -10,6 +28,8 @@ export default class ClickOutside extends Component {
     super(props)
     this.getContainer = this.getContainer.bind(this)
     this.isTouch = false
+    this.posStart = { x: 0, y: 0 };
+    this.posEnd = { x: 0, y: 0 };
   }
 
   getContainer(ref) {
@@ -22,18 +42,32 @@ export default class ClickOutside extends Component {
   }
 
   componentDidMount() {
+    document.addEventListener('touchstart', this.onTouchStart, true)
     document.addEventListener('touchend', this.handle, true)
     document.addEventListener('click', this.handle, true)
   }
 
   componentWillUnmount() {
+    document.removeEventListener('touchstart', this.onTouchStart, true)
     document.removeEventListener('touchend', this.handle, true)
     document.removeEventListener('click', this.handle, true)
   }
 
+  onTouchStart = e => {
+    setPos(this.posStart, e)
+  }
+
   handle = e => {
-    if (e.type === 'touchend') this.isTouch = true
-    if (e.type === 'click' && this.isTouch) return
+    const type = e.type;
+    let canClick = false;
+
+    if (type === 'touchend') {
+      this.isTouch = true;
+      setPos(this.posEnd, e)
+      canClick = inThreshold(this.posEnd, this.posStart)
+    }
+
+    if ((type === 'click' && this.isTouch) || !canClick) return;
     const { onClickOutside } = this.props
     const el = this.container
     if (!el.contains(e.target)) onClickOutside(e)
